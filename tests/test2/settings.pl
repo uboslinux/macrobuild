a0a84364c724239711888ad2adc5917c52a160a7
Johannes Ernst <jernst@indiecomputing.com>
2017-11-01 21:46:53 +0000

    Pass on arguments through alias resolution

--- tests/test2/settings.pl
+++ tests/test2/settings.pl
@@ -1,7 +1,9 @@
 my $settings = {
     'shortcuts' => {
-        'something'      => 'else',
-        'search-replace' => [ 'TestTasks::SearchReplace', 'pattern=a', 'replacement=A' ]
+        'something'                       => 'else',
+        'search-replace-with'             => [ 'TestTasks::SearchReplace', 'pattern=a', 'replacement=A' ],
+        'search-replace-without'          => [ 'TestTasks::SearchReplace' ],
+        'search-replace-without-indirect' => [ 'search-replace-without' ]
     }
 };
 
--- tests/test2/test2-macros.pl
+++ tests/test2/test2-macros.pl
@@ -4,7 +4,7 @@ use strict;
 use warnings;
 
 use UBOS::Utils;
-use Test::More tests => 3;
+use Test::More tests => 7;
 
 # Definitions
 
@@ -14,11 +14,27 @@ my $out;
 # Test Replace 1
 
 is( myexec( "$macrobuild -i Test2.in"
-            . " search-replace",
+            . " search-replace-with",
             undef, \$out, \$out ), 0, "test2-replace1-a" );
 
 like( $out, qr/Abc/, "test2-replace1-b" );
 
 unlike( $out, qr/Nothing to do/, "test2-replace1-c" );
+
+# Test Replace 2
+
+is( myexec( "$macrobuild -i Test2.in"
+            . " search-replace-without pattern=a replacement=AXA",
+            undef, \$out, \$out ), 0, "test2-replace2-a" );
+
+like( $out, qr/AXAbc/, "test2-replace2-b" );
+
+# Test Replace 3
+
+is( myexec( "$macrobuild -i Test2.in"
+            . " search-replace-without-indirect pattern=a replacement=AXA",
+            undef, \$out, \$out ), 0, "test2-replace3-a" );
+
+like( $out, qr/AXAbc/, "test2-replace3-b" );
 
 1;
